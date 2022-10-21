b0b1d482925a439455850f47291e0bc8342fac22
serhad-e <86155806+serhad-e@users.noreply.github.com>
2022-10-21 20:38:46 +0300

    Add files via upload

---
+++ duet.php
@@ -0,0 +1,129 @@
+<?php include("kutuphane/ust.php"); ?>
+<section class="container pt-0 mt-lg-3 mt-xl-4 mt-xxl-5">
+  <h2 class="h1 text-center pt-2 pt-sm-3 pt-md-4 pt-xl-5 mt-lg-2 mt-xl-1">Düğün Ve Nişan</h2>
+  <p class="text-center pb-3 mb-3 mb-lg-4">Düğün Ve Nişan Törenlerinizi Bizimle Beraber Planlayın</p>
+  <div class="row row-cols-1 row-cols-xl-2 ">
+    <!-- Item-->
+    <video class="w-100" autoplay loop muted>
+      <source src="assets/video/dugun/dugun1.mp4" type="video/mp4" />
+    </video>
+    <!-- Item-->
+    <!-- Item-->
+    <div class="card border-0 bg-transparent h-100">
+      <div class="card-body d-flex justify-content-center align-items-center border-dashed border-2 rounded-3 py-5 mb-3">
+        <iframe width="600" height="700" src="https://www.youtube.com/embed/5Bw-WMNbjOY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      </div>
+    </div>
+
+    <!-- Item-->
+    <!-- Item-->
+    <div class="card border-0 bg-transparent h-100">
+      <div class="card-body d-flex justify-content-center align-items-center border-dashed border-2 rounded-3 py-5 mb-3">
+      <iframe width="600" height="700" src="https://www.youtube.com/embed/G0j_ksn9t20" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  </div>
+    <!-- Item-->
+    <!-- Item-->
+    <div class="card border-0 bg-transparent h-100">
+      <div class="card-body d-flex justify-content-center align-items-center border-dashed border-2 rounded-3 py-5 mb-3">
+      <video width="600" height="700" controls>
+    <source src="assets/video/dugun/dugun4.mp4" type="video/mp4">
+    <source src="assets/video/dugun/dugun4.mp4" type="video/ogg">
+    </video>
+    </div>
+  </div>
+    <!-- Item-->
+    <!-- Item-->
+    <div class="card border-0 bg-transparent h-100">
+      <div class="card-body d-flex justify-content-center align-items-center border-dashed border-2 rounded-3 py-5 mb-3">
+      <video width="600" height="700" controls>
+    <source src="assets/video/dugun/dugun5.mp4" type="video/mp4">
+    <source src="assets/video/dugun/dugun5.mp4" type="video/ogg">
+    </video>
+    </div>
+  </div>
+    <!-- Item-->
+    <!-- Item-->
+    <div class="card border-0 bg-transparent h-100">
+      <div class="card-body d-flex justify-content-center align-items-center border-dashed border-2 rounded-3 py-5 mb-3">
+      <video width="600" height="700" controls>
+    <source src="assets/video/dugun/dugun6.mp4" type="video/mp4">
+    <source src="assets/video/dugun/dugun6.mp4" type="video/ogg">
+    </video>
+    </div>
+  </div>
+    <!-- Item-->
+    <!-- Item-->
+    <div class="card border-0 bg-transparent h-100">
+      <div class="card-body d-flex justify-content-center align-items-center border-dashed border-2 rounded-3 py-5 mb-3">
+      <video width="600" height="700" controls>
+    <source src="assets/video/dugun/dugun7.mp4" type="video/mp4">
+    <source src="assets/video/dugun/dugun7.mp4" type="video/ogg">
+    </video>
+    </div>
+  </div>
+    <!-- Item-->
+    <!-- Item-->
+    <div class="card border-0 bg-transparent h-100">
+      <div class="card-body d-flex justify-content-center align-items-center border-dashed border-2 rounded-3 py-5 mb-3">
+      <video width="600" height="700" controls>
+    <source src="assets/video/dugun/dugun9.mp4" type="video/mp4">
+    <source src="assets/video/dugun/dugun9.mp4" type="video/ogg">
+    </video>
+    </div>
+  </div>
+    <!-- Item-->
+    <!-- Item-->
+    <div class="card border-0 bg-transparent h-100">
+      <div class="card-body d-flex justify-content-center align-items-center border-dashed border-2 rounded-3 py-5 mb-3">
+      <video width="600" height="700" controls>
+    <source src="assets/video/dugun/dugun10.mp4" type="video/mp4">
+    <source src="assets/video/dugun/dugun10.mp4" type="video/ogg">
+    </video>
+    </div>
+  </div>
+    <!-- Item-->
+    <!-- Item-->
+    <div class="card border-0 bg-transparent h-100">
+      <div class="card-body d-flex justify-content-center align-items-center border-dashed border-2 rounded-3 py-5 mb-3">
+      <video width="600" height="700" controls>
+    <source src="assets/video/dugun/dugun11.mp4" type="video/mp4">
+    <source src="assets/video/dugun/dugun11.mp4" type="video/ogg">
+    </video>
+    </div>
+  </div>
+    <!-- Item-->
+    <!-- Item-->
+    <div class="card border-0 bg-transparent h-100">
+      <div class="card-body d-flex justify-content-center align-items-center border-dashed border-2 rounded-3 py-5 mb-3">
+      <video width="600" height="700" controls>
+    <source src="assets/video/dugun/dugun12.mp4" type="video/mp4">
+    <source src="assets/video/dugun/dugun12.mp4" type="video/ogg">
+    </video>
+    </div>
+  </div>
+    <!-- Item-->
+    <div class="card border-0 bg-transparent h-100">
+      <div class="card-body d-flex justify-content-center align-items-center border-dashed border-2 rounded-3 py-5 mb-3">
+      <video width="600" height="700" controls>
+    <source src="assets/video/dugun/dugun13.mp4" type="video/mp4">
+    <source src="assets/video/dugun/dugun13.mp4" type="video/ogg">
+    </video>
+    </div>
+  </div>
+
+
+    <div class="col">
+      <div class="card border-0 bg-transparent h-100">
+        <div class="card-body d-flex justify-content-center align-items-center border-dashed border-2 rounded-3 py-5 mb-3">
+          <div class="py-5" style="width: 5.5rem;">
+            <lottie-player class="d-dark-mode-none" src="assets/json/animation-sandwatch-light.json" background="transparent" speed="1" loop="" autoplay=""></lottie-player>
+            <lottie-player class="d-none d-dark-mode-block" src="assets/json/animation-sandwatch-dark.json" background="transparent" speed="1" loop="" autoplay=""></lottie-player>
+          </div>
+        </div>
+        <h3 class="h5 text-nav text-center mb-0">Loading...</h3>
+      </div>
+    </div>
+  </div>
+</section>
+<?php include("kutuphane/alt.php"); ?>
---
+++ index.php
@@ -0,0 +1,106 @@
+<?php include("kutuphane/ust.php"); ?>
+
+      <section class="bg-dark dark-mode position-relative min-vh-100 overflow-hidden pt-5">
+        <svg class="position-absolute top-0" width="262" height="160" viewbox="0 0 262 160" fill="none" xmlns="http://www.w3.org/2000/svg" style="left: 6%;">
+          <ellipse cx="131" cy="30.5" rx="131" ry="129.5" fill="white" fill-opacity=".03"></ellipse>
+        </svg>
+        <div class="container position-relative zindex-5 text-center pt-5 mt-lg-4 mt-xl-5">
+          <h1 class="display-1 pt-4 mt-sm-3"><span class="text-white fw-normal">Bejan Berken <span class="d-inline-flex align-items-center"><img class="flex-shrink-0 mt-2" src="assets/img/landing/intro/bejan.jpg" width="100" alt="Image"></span>fficial<span class="d-inline-flex align-items-center"></h1>
+          <p class="fs-xl mb-5">Organizasyonlarınız Bizimle, Biz Sizinle Özeliz</p><a class="scroll-down-btn" href="#landings" data-scroll="" data-scroll-offset="20">
+            <div class="mouse"></div>
+            <p font-size:45px>Aşağı Kaydır</p></a>
+        </div>
+        <div class="d-none d-lg-block" style="margin-top: -80px;"></div>
+        <div class="d-none d-sm-block d-lg-none" style="margin-top: -40px;"></div>
+        <div class="parallax mx-auto mb-n5" style="max-width: 1606px;">
+          <div class="parallax-layer" data-depth="-0.1"><img src="assets/img/landing/intro/hero/01.png" alt="Background shapes"></div>
+          <div class="parallax-layer zindex-2" data-depth="0.12"><img src="assets/img/landing/intro/hero/bbm4.jpg" alt="Foreground shape"></div>
+          <div class="parallax-layer zindex-2" data-depth="0.35"><img src="" alt=""></div>
+          <div class="parallax-layer zindex-2" data-depth="0.35"><img src="" alt=""></div>
+        </div>
+      </section>
+      <!-- Landings-->
+      <section class="bg-gray py-2 py-lg-3 py-xl-4 py-xxl-5" id="landings">
+        <div class="container py-5 mt-1 mb-2 mt-sm-2 mb-sm-3 mt-md-3 mb-md-4 my-lg-4 my-xl-5">
+          <h2 class="display-5 text-center pt-xxl-2">Konseptler</h2>
+          <p class="fs-lg text-center pb-3 pb-lg-0 mb-lg-5">Siz Seçin, Biz Uygulayalım</p>
+          <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 gy-5 pb-xl-3">
+            <div class="col">
+              <div class="card-hover card-lifted position-relative mx-auto" style="max-width: 416px;">
+                <div class="card-lifted overflow-hidden rounded-3 mb-3"><img class="d-dark-mode-none" src="assets/img/landing/intro//cmfoto1.jpg" alt="SaaS v.1"><img class="d-none d-dark-mode-block" src="assets/img/landing/intro//cmfoto1.jpg" alt="SaaS v.1"></div>
+                <h3 class="h5 text-center mb-0"><a class="stretched-link" href="cm.php">Canlı Müzik</a></h3>
+              </div>
+            </div>
+            <!-- Mobile App Showcase-->
+            <div class="col ">
+              <div class="card-hover card-lifted position-relative mx-auto" style="max-width: 416px;">
+                <div class="card-lifted overflow-hidden rounded-3 mb-3"><img class="d-dark-mode-none" src="assets/img/landing/intro//dugunfoto1.jpg" alt="Mobile App Showcase"><img class="d-none d-dark-mode-block" src="assets/img/landing/intro//dugunfoto1.jpg" alt="Düğün"></div>
+                <h3 class="h5 text-center mb-0"><a class="stretched-link" href="dugun.php">Düğün Törenleri</a></h3>
+              </div>
+            </div>
+            <!-- Product Landing-->
+            <div class="col">
+              <div class="card-hover card-lifted position-relative mx-auto" style="max-width: 416px;">
+                <div class="card-lifted overflow-hidden rounded-3 mb-3"><img class="d-dark-mode-none" src="assets/img/landing/intro/canlımuzik2.jpg" alt="Product Landing"><img class="d-none d-dark-mode-block" src="assets/img/landing/intro/canlımuzik2.jpg" alt="Canlı Müzik"></div>
+                <h3 class="h5 text-center mb-0"><a class="stretched-link" href="album.php">Fotoğraf & Video Albümü</a></h3>
+              </div>
+            </div>
+            <!-- SaaS v.1-->
+
+            <!-- Corporate-->
+            <div class="col">
+              <div class="card-hover card-lifted position-relative mx-auto" style="max-width: 416px;">
+                <div class="card-lifted overflow-hidden rounded-3 mb-3"><img class="d-dark-mode-none" src="assets/img/landing/intro/landings/corporate-light.jpg" alt="Corporate"><img class="d-none d-dark-mode-block" src="assets/img/landing/intro/landings/corporate-dark.jpg" alt="Corporate"></div>
+                <h3 class="h5 text-center mb-0"><a class="stretched-link" href="duet.php">Düet Çalışmaları</a></h3>
+              </div>
+            </div>
+
+            <!-- Coming Soon-->
+            <div class="col">
+              <div class="card border-0 bg-transparent h-100">
+                <div class="card-body d-flex justify-content-center align-items-center border-dashed border-2 rounded-3 py-5 mb-3">
+                  <div class="py-5" style="width: 5.5rem;">
+                    <lottie-player class="d-dark-mode-none" src="assets/json/animation-sandwatch-light.json" background="transparent" speed="1" loop="" autoplay=""></lottie-player>
+                    <lottie-player class="d-none d-dark-mode-block" src="assets/json/animation-sandwatch-dark.json" background="transparent" speed="1" loop="" autoplay=""></lottie-player>
+                  </div>
+                </div>
+                <h3 class="h5 text-nav text-center mb-0">Loading</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Inner pages-->
+      <section class="overflow-hidden pt-2 pt-sm-3 pt-md-4 pt-lg-3 pb-1 pb-sm-3 pb-md-4 pb-lg-5">
+        <div class="container position-relative py-5 my-lg-3 my-xl-4 my-xxl-5">
+          <div class="position-absolute" style="margin-left: -12.7%;" data-aos="zoom-in" data-aos-duration="500" data-aos-offset="300" data-disable-parallax-down="lg">
+            <svg class="text-warning d-none d-lg-block" width="744" height="744" viewbox="0 0 744 744" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <circle cx="372" cy="372" r="372"></circle>
+            </svg>
+          </div>
+          <div class="d-none d-xxl-block" style="height: 190px;"></div>
+          <div class="d-none d-lg-block d-xxl-none" style="height: 150px;"></div>
+          <div class="row justify-content-center justify-content-lg-start position-relative zindex-2">
+            <div class="col-sm-10 col-md-8 col-lg-5 col-xl-4 text-center text-lg-start" data-aos="fade-up" data-aos-duration="850" data-aos-offset="120" data-disable-parallax-down="lg">
+              <div class="ignore-dark-mode d-none d-lg-block">
+                <h2 class="display-5 mb-4">Inner pages for everything you may need</h2>
+                <p class="fs-lg text-dark pb-3 pb-sm-4 mb-lg-0">Big collection of thoroughly designed inner page templates from User Account/Dashboard to Specialty pages like 404 and Coming Soon.</p>
+              </div>
+              <div class="d-lg-none">
+                <h2 class="display-5 mb-4">Inner pages for everything you may need</h2>
+                <p class="fs-lg text-dark pb-3 pb-sm-4 mb-lg-0">Big collection of thoroughly designed inner page templates from User Account/Dashboard to Specialty pages like 404 and Coming Soon.</p>
+              </div>
+            </div>
+          </div>
+          <div class="d-none d-xl-block" style="margin-top: -240px;"></div>
+          <div class="d-none d-lg-block d-xl-none" style="margin-top: -160px;"></div>
+          <div class="d-flex position-relative zindex-2 mb-3">
+            <div class="flex-xxl-shrink-0 position-relative zindex-2" style="max-width: 540px; margin-top: 26%; margin-right: -9.6%;" data-aos="fade-up" data-aos-duration="500" data-aos-delay="400" data-disable-parallax-down="lg"><img class="rounded-3 mt-n5 mt-lg-0 d-block d-dark-mode-none" src="assets/img/landing/intro/pages/foto.jpeg" alt="Page" style="box-shadow: 1.875rem .625rem 5rem rgba(65,81,98, .2);"><img class="border rounded-3 mt-n5 mt-lg-0 d-none d-dark-mode-block" src="assets/img/landing/intro/pages/foto.jpeg" alt="Page" style="box-shadow: 1.875rem .625rem 5rem rgba(0,0,0, .2);"></div>
+            <div class="flex-xxl-shrink-0 position-relative zindex-1" style="max-width: 481px; margin-top: 17.6%; margin-right: -8.5%;" data-aos="fade-up" data-aos-duration="500" data-aos-delay="200" data-disable-parallax-down="lg"><img class="rounded-3 mt-n5 mt-lg-0 d-block d-dark-mode-none" src="assets/img/landing/intro/pages/foto1.jpeg" alt="Page" style="box-shadow: 1.875rem .625rem 5rem rgba(65,81,98, .2);"><img class="border rounded-3 mt-n5 mt-lg-0 d-none d-dark-mode-block" src="assets/img/landing/intro/pages/foto1.jpeg" alt="Page" style="box-shadow: 1.875rem .625rem 5rem rgba(0,0,0, .2);"></div>
+            <div class="flex-xxl-shrink-0" style="max-width: 510px;" data-aos="fade-up" data-aos-duration="500" data-aos-offset="250" data-disable-parallax-down="lg"><img class="rounded-3 d-block d-dark-mode-none" src="assets/img/landing/intro/pages/03-light.jpg" alt="Page" style="box-shadow: 1.875rem .625rem 5rem rgba(65,81,98, .2);"><img class="border rounded-3 d-none d-dark-mode-block" src="assets/img/landing/intro/pages/03-dark.jpg" alt="Page" style="box-shadow: 1.875rem .625rem 5rem rgba(0,0,0, .2);"></div>
+          </div>
+        </div>
+      </section>
+        </div>
+      </section>
+<?php include("kutuphane/alt.php"); ?>
